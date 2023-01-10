902a9445f652e59caf7735b74e23198202b52e16
vvaldelamar <vvaldelamar@gmail.com>
2023-01-10 13:05:29 -0600

    Update login.php

--- action/login.php
+++ action/login.php
@@ -3,11 +3,11 @@
 	session_start();
         //validacion de CSRF 
 
-     	if (isset($_POST[hash_equals($_SESSION['tokens'],$_POST['anticsrf'])] ) )
-	{	
+if (isset($_POST[hash_equals($_SESSION['tokens'],$_POST['anticsrf'])] ) )
  	unset($_SESSION['tokens']);	
 	
-	if (isset($_POST['token']) && $_POST['token']!=='') {
+  if (isset($_POST['token']) && $_POST['token']!=='') 
+   {
 
         //Contiene las variables de configuracion para conectar a la base de datos
         include "../config/config.php";
@@ -41,15 +41,12 @@
                 }
              }else{  header("location: ../");    }
 
-	 }                    
-
-	
-	}else
-		 {
-		unset($_SESSION['tokens']);
-		$recap=sha1(md5("recaptcha"));
-         	header("location: ../index.php?recap=$recap");
-		exit;
-		}	
-
+    } else
+        {
+            unset($_SESSION['tokens']);
+            $recap=sha1(md5("recaptcha"));
+                header("location: ../index.php?recap=$recap");
+            exit;
+ 
+        }
 ?>
